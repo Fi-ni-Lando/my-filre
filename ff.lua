b9ee8ac9c8aa3ee87ae81c3be9d3c4f4df53d264
Elijah <elijahsalunga365@gmail.com>
2025-10-26 23:10:32 +0900

    Add files via upload

---
+++ ff.lua
@@ -0,0 +1,14 @@
+Z쁩Rzqk'ZjTn''Rzqn''KԞzxgIǔȞIǥƥZϕqOZjXek+%ƥzDykhqgjgZ+
+hwdF_:ZjYf
+h*%{
+uZjW*iyШ¢Z+۞{ӽ%ƥ~ܶ*'Z''׬InWnwvZjWbZ)z{h ⲙZek'جVnwvZjXzWbZVzYhqb鬖ZŊwE-
+t'i)i-ߢb*Zi)izСWkzwhhR,:(^֛觱צ(֦yzw^؟Vzjߢb*Z)(Z-z(bޝ"~(�N%z)zb~rh۞{ӽ-Mjg詥~zw^קv)-ޕ7+^ZnWw+^-lzweƥ~ܶ*'r'Hek'rبǭbVj\r׫ek+)^)z{h ⲙZek'zk(ek+)xzweƥri^fZ+
+twwaymz۫ݖ'֧qѮzuԢm{
+5M9Ѹԭ}W4۝Y[\çNm益Y[v^'i^uڭ햇Zj+EWOkvxzR'֧q7zYڙmR7)5Mv-ޔ
+wS쥪ޝ̵-ޕ7ZjgڙmS{BZ+vx*%ڙmE{D酢{Ry&%vvxzTԢu-ޔz{[Y[ݖ)-ޔ쵩zwMm-ޖ)-ޕ(y@i_|{i>"*'P8w}5)-ޔ
+wS쥪ޝ̵{iMm
+hyf
+h)-ޔZ'IZ'*.ǒj{"mS{R7)-ޕ7p%	z{D{WXg)-ޕ7%	z{D{XXg:){iF'!Mm{i=ޞVjb+)Vz.Zmbmeƥ~ܶ*'zSlrv'܆)]"m֭xz{h*ek'zw^!j)]^t*'-zSlri^(b^t*'-zSlZŊwE-
+t'i)i-i)i)]^t*'-zSlm$$
+u
+yky޲[קvZjW!j镬(Z-z+VzjuםYn!j镬(Z-zuByrbw!jZL9n!juݖrب#镬ZjWbZ)z{h ⲙZek'bZ^جVzk(ek+)xzw^hq_w-%xDzDy)v_݊ek()z{h ⲙZhbZv+)!'i^u)v^קuRǫ"zn''"znjpwrب)kǬb~q,yazz޶zwb~),z'Sʗ锱ȞO*^)웡ݶJ{ u'b*xaz{h	^#ޝקu~Vzk'g>Vz'镬ZjSk'r'Hek'Z'zwOk'^t*'-~ܶ*'Vz+y^"{ޞ+DVztZ>Vz^�w#ѺtxwjШwrب'׬InWnwu+Z*ZZvVzhqO^'اek+)^j׈#镬ݺZ'zw^
